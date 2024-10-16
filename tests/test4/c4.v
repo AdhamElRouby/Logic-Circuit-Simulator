1294c367cf76175a77c9934a2be7441794ab50ca
mahmoudhossam532 <mahmoudhossam@aucegypt.edu>
2024-10-16 17:16:26 +0300

    all tests done

--- tests/test4/c4.v
+++ tests/test4/c4.v
@@ -1,6 +1,6 @@
 module c4 (input i2,i1,i0,output o);
 wire w1,w0;
-xor #(1)(w1,i2,i1);
-not #(1)(w0,i0);
-and #(1)(o,w1,w0);
+xor #(1) g1 (w1,i2,i1);
+not #(1) g2 (w0,i0);
+and #(1) g3 (o,w1,w0);
 endmodule 
