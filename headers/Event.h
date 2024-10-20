66dd0336b3a3c374fd4fa3b799dbeb08645ed0fe
mahmoudhossam532 <mahmoudhossam@aucegypt.edu>
2024-10-21 01:03:40 +0300

    done class Gate and Event

--- headers/Event.h
+++ headers/Event.h
@@ -1,12 +1,17 @@
 #ifndef EVENT
 #define EVENT
-
+#include <string>
+using namespace std;
 class Event {
     private:
-
-
+    int time;
+    string name;
+    int newValue;
     public:
-    
+    Event(int t,string n,int nv);//time,name,newValue
+    int getTime();
+    string getName();
+    int getNewValue();
 };
 
 
